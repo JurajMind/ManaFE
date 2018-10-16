bbef8001689f4276b39c149d71ed4085cbc32718
Juraj Hamornik <hamornik@manapipes.com>
2018-10-16 22:26:14 +0200

    rp

--- lib/pages/Places/reservation_page.dart
+++ lib/pages/Places/reservation_page.dart
@@ -29,119 +29,126 @@ class _ReservationPageState extends State<ReservationPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(20.0), color: Colors.white),
+    return SafeArea(
       child: Theme(
         isMaterialAppTheme: true,
         data: ThemeData.light(),
-        child: new Column(
-          mainAxisSize: MainAxisSize.max,
-          children: <Widget>[
-            new AppBar(
-              title: new Text('Book place'),
-              backgroundColor: Colors.white,
-            ),
-            Flexible(
-              child: ListView(
-                scrollDirection: Axis.vertical,
-                children: <Widget>[
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: <Widget>[
-                      Card(
-                        child: new Calendar(
-                          isExpandable: true,
-                          onDateSelected: (date) {
-                            print(date.toString());
-                          },
-                        ),
-                      ),
-                      Card(
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          mainAxisSize: MainAxisSize.max,
-                          children: <Widget>[
-                            Padding(
-                              padding:
-                                  EdgeInsets.fromLTRB(10.0, 4.0, 10.0, 2.0),
-                              child: Column(
-                                children: <Widget>[
-                                  Text(
-                                    'Peoples',
-                                    style: TextStyle(color: Colors.grey),
-                                  ),
-                                  WheelPicker.integer(
-                                    initialValue: peopleCount,
-                                    minValue: 1,
-                                    maxValue: 10,
-                                    onChanged: (value) {
-                                      print(value);
-                                      setState(() {
-                                        peopleCount = value;
-                                      });
-                                    },
-                                  )
-                                ],
-                              ),
-                            ),
-                            Padding(
-                              padding:
-                                  EdgeInsets.fromLTRB(10.0, 4.0, 10.0, 2.0),
-                              child: Column(
-                                children: <Widget>[
-                                  Text(
-                                    'Time',
-                                    style: TextStyle(color: Colors.grey),
-                                  ),
-                                  WheelPicker.string(
-                                    initialValue: selectedTime,
-                                    minValue: 1,
-                                    maxValue: 4,
-                                    stringItems: [
-                                      "18:00",
-                                      "18:30",
-                                      "19:00",
-                                      "19:30"
-                                    ],
-                                    onChanged: (value) {
-                                      print(value);
-                                      setState(() {
-                                        selectedTime = value;
-                                      });
-                                    },
-                                  )
-                                ],
-                              ),
-                            )
-                          ],
-                        ),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.symmetric(
-                            horizontal: 8.0, vertical: 0.0),
-                        child: new RoundedButton(
-                          buttonName: 'Reserve',
-                          onTap: () {},
-                          buttonColor: Colors.black,
-                          borderWidth: 0.0,
-                          bottomMargin: 0.0,
-                          height: 40.0,
-                          width: 200.0,
-                        ),
-                      ),
-                    ],
-                  ),
-                  Container(
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(20.0),
-                    ),
-                  )
-                ],
+        child: Container(
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(8.0), color: Colors.white),
+          child: new Column(
+            mainAxisSize: MainAxisSize.max,
+            children: <Widget>[
+              new AppBar(
+                iconTheme: IconThemeData(
+                  color: Colors.black, //change your color here
+                ),
+                title: new Text(
+                  'Book place',
+                  style: TextStyle(color: Colors.black),
+                ),
+                backgroundColor: Colors.white,
               ),
-            ),
-          ],
+              Flexible(
+                child: ListView(
+                  scrollDirection: Axis.vertical,
+                  children: <Widget>[
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: <Widget>[
+                        Card(
+                          child: new Calendar(
+                            isExpandable: true,
+                            onDateSelected: (date) {
+                              print(date.toString());
+                            },
+                          ),
+                        ),
+                        Card(
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            mainAxisSize: MainAxisSize.max,
+                            children: <Widget>[
+                              Padding(
+                                padding:
+                                    EdgeInsets.fromLTRB(10.0, 4.0, 10.0, 2.0),
+                                child: Column(
+                                  children: <Widget>[
+                                    Text(
+                                      'Peoples',
+                                      style: TextStyle(color: Colors.grey),
+                                    ),
+                                    WheelPicker.integer(
+                                      initialValue: peopleCount,
+                                      minValue: 1,
+                                      maxValue: 10,
+                                      onChanged: (value) {
+                                        print(value);
+                                        setState(() {
+                                          peopleCount = value;
+                                        });
+                                      },
+                                    )
+                                  ],
+                                ),
+                              ),
+                              Padding(
+                                padding:
+                                    EdgeInsets.fromLTRB(10.0, 4.0, 10.0, 2.0),
+                                child: Column(
+                                  children: <Widget>[
+                                    Text(
+                                      'Time',
+                                      style: TextStyle(color: Colors.grey),
+                                    ),
+                                    WheelPicker.string(
+                                      initialValue: selectedTime,
+                                      minValue: 1,
+                                      maxValue: 4,
+                                      stringItems: [
+                                        "18:00",
+                                        "18:30",
+                                        "19:00",
+                                        "19:30"
+                                      ],
+                                      onChanged: (value) {
+                                        print(value);
+                                        setState(() {
+                                          selectedTime = value;
+                                        });
+                                      },
+                                    )
+                                  ],
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.all(10.0),
+                          child: new RoundedButton(
+                            buttonName: 'Reserve',
+                            onTap: () {},
+                            buttonColor: Colors.black,
+                            borderWidth: 0.0,
+                            bottomMargin: 0.0,
+                            height: 40.0,
+                            width: 200.0,
+                          ),
+                        ),
+                      ],
+                    ),
+                    Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20.0),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
