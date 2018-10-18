25ecca01e189b6f2a8c0dbdd887594a6e1c4b259
Juraj Hamornik <jurko@bdi.sk>
2018-10-18 14:54:28 +0200

    placeSliver

--- lib/pages/Places/places.page.dart
+++ lib/pages/Places/places.page.dart
@@ -56,52 +56,34 @@ class _PlacePageState extends State<PlacePage> {
 
   @override
   Widget build(BuildContext context) {
+    var height = MediaQuery.of(context).size.height / 2;
     staticMapUri = staticMapProvider.getStaticUri(
         placeBloc.location?.value ?? myUserLocation, 14,
-        width: 450, height: 250, mapType: StaticMapViewType.roadmap);
+        width: 450, height: height.round(), mapType: StaticMapViewType.roadmap);
 //https://maps.googleapis.com/maps/api/staticmap?center=${myUserLocation.altitude},${myUserLocation.longitude}&zoom=15&format=png&maptype=roadmap&style=element:geometry%7Ccolor:0x1d2c4d&style=element:labels.text.fill%7Ccolor:0x8ec3b9&style=element:labels.text.stroke%7Ccolor:0x1a3646&style=feature:administrative.country%7Celement:geometry.stroke%7Ccolor:0x4b6878&style=feature:administrative.land_parcel%7Celement:labels.text.fill%7Ccolor:0x64779e&style=feature:administrative.province%7Celement:geometry.stroke%7Ccolor:0x4b6878&style=feature:landscape.man_made%7Celement:geometry.stroke%7Ccolor:0x334e87&style=feature:landscape.natural%7Celement:geometry%7Ccolor:0x023e58&style=feature:poi%7Celement:geometry%7Ccolor:0x283d6a&style=feature:poi%7Celement:labels.text.fill%7Ccolor:0x6f9ba5&style=feature:poi%7Celement:labels.text.stroke%7Ccolor:0x1d2c4d&style=feature:poi.business%7Cvisibility:off&style=feature:poi.park%7Celement:geometry.fill%7Ccolor:0x023e58&style=feature:poi.park%7Celement:labels.text%7Cvisibility:off&style=feature:poi.park%7Celement:labels.text.fill%7Ccolor:0x3C7680&style=feature:road%7Celement:geometry%7Ccolor:0x304a7d&style=feature:road%7Celement:labels.text.fill%7Ccolor:0x98a5be&style=feature:road%7Celement:labels.text.stroke%7Ccolor:0x1d2c4d&style=feature:road.highway%7Celement:geometry%7Ccolor:0x2c6675&style=feature:road.highway%7Celement:geometry.stroke%7Ccolor:0x255763&style=feature:road.highway%7Celement:labels.text.fill%7Ccolor:0xb0d5ce&style=feature:road.highway%7Celement:labels.text.stroke%7Ccolor:0x023e58&style=feature:transit%7Celement:labels.text.fill%7Ccolor:0x98a5be&style=feature:transit%7Celement:labels.text.stroke%7Ccolor:0x1d2c4d&style=feature:transit.line%7Celement:geometry.fill%7Ccolor:0x283d6a&style=feature:transit.station%7Celement:geometry%7Ccolor:0x3a4762&style=feature:water%7Celement:geometry%7Ccolor:0x0e1626&style=feature:water%7Celement:labels.text.fill%7Ccolor:0x4e6d70&size=900x500&key${App.googleApiKeys}
 
     return SafeArea(
-      child: new Column(
-        mainAxisSize: MainAxisSize.max,
-        children: <Widget>[
-          Expanded(
-            child: Stack(
-              children: <Widget>[
-                Positioned(top: 20.0, child: mapBuilder()),
-                Positioned(
-                    child: SizedBox(
-                  height: 50.0,
-                  child: new AppBar(
-                    leading: BackButtonIcon(),
-                    actions: <Widget>[
-                      IconButton(
-                        icon: Icon(Icons.search),
-                        onPressed: () => print('search'),
-                      )
-                    ],
-                    title: new Text('Place'),
-                    centerTitle: true,
-                    bottomOpacity: 0.0,
-                    backgroundColor: Colors.transparent,
-                    automaticallyImplyLeading: true,
-                  ),
-                ))
-              ],
+      child: new CustomScrollView(        
+        slivers: <Widget>[
+          SliverAppBar(
+            backgroundColor: Colors.transparent,
+            expandedHeight:height,
+            title: Text('Nearby places'),
+            centerTitle: true,
+            actions: <Widget>[
+              IconButton(
+                icon: Icon(Icons.search),
+              )
+            ],
+            flexibleSpace: new FlexibleSpaceBar(
+              collapseMode: CollapseMode.parallax,
+              background: Container(
+                height: height,
+                child: mapBuilder()),
             ),
-            flex: 1,
+            
           ),
-          Expanded(
-            child: StreamBuilder<bool>(
-                stream: placeBloc.loading,
-                initialData: true,
-                builder: (context, snapshot) {
-                  return snapshot.data
-                      ? Center(child: CircularProgressIndicator())
-                      : placeBuilder(placeBloc.places);
-                }),
-            flex: 1,
-          )
+          placeBuilder(placeBloc.places)
         ],
       ),
     );
@@ -112,10 +94,11 @@ class _PlacePageState extends State<PlacePage> {
         stream: places,
         initialData: null,
         builder: (context, snapshot) {
-          return ListView.builder(
-            itemCount: snapshot.data == null ? 0 : snapshot.data.length,
-            itemBuilder: (context, index) =>
-                _createPlaceItem(index, snapshot.data[index]),
+          return SliverList(
+            delegate: SliverChildBuilderDelegate((context,index){
+               return _createPlaceItem(index, snapshot.data[index]);
+            },
+            childCount: snapshot.data == null ? 0 : snapshot.data.length, ),
           );
         });
   }
