fa84fe8fabbbe28d8924e84232b42d052f45b8ab
Juraj Hamornik <hamornik@manapipes.com>
2019-01-17 01:06:57 +0100

    color bg

--- lib/components/Common/bg_painter.dart
+++ lib/components/Common/bg_painter.dart
@@ -1,14 +1,58 @@
-
-
 import 'package:flutter/material.dart';
-import 'package:tinycolor/tinycolor.dart';
 
 class BgPainter extends CustomPainter {
+  final Color color;
+  final Offset startPoint;
 
-final Color color;
+  static const List<Offset> logoPoints = [
+    Offset(-120.546, 76.496),
+    Offset(-118.296, 93.169),
+    Offset(-116.047, 109.842),
+    Offset(-113.798, 126.515),
+    Offset(-111.549, 143.188),
+    Offset(-109.3, 159.861),
+    Offset(-107.051, 176.534),
+    Offset(-104.802, 193.207),
+    Offset(-102.552, 209.879),
+    Offset(-100.303, 226.552),
+    Offset(-79.016, 226.405),
+    Offset(-57.729, 226.258),
+    Offset(-36.442, 226.11),
+    Offset(-15.155, 225.963),
+    Offset(6.132, 225.816),
+    Offset(27.419, 225.668),
+    Offset(48.706, 225.521),
+    Offset(69.993, 225.374),
+    Offset(91.28, 225.226),
+    Offset(95.249, 208.027),
+    Offset(99.219, 190.828),
+    Offset(103.188, 173.629),
+    Offset(107.157, 156.429),
+    Offset(111.126, 139.23),
+    Offset(115.095, 122.031),
+    Offset(119.064, 104.832),
+    Offset(123.034, 87.633),
+    Offset(127.003, 70.433),
+    Offset(112.891, 62.607),
+    Offset(98.78, 54.781),
+    Offset(84.668, 46.955),
+    Offset(70.557, 39.129),
+    Offset(56.446, 31.303),
+    Offset(42.334, 23.477),
+    Offset(28.223, 15.651),
+    Offset(14.111, 7.825),
+    Offset(0, 0),
+  ];
 
-  BgPainter({this.color = Colors.orange});
+  var hueRotation;
 
+  BgPainter(
+      {this.color = const Color(0xFF1F03FF),
+      this.logoSize = 0.4,
+      this.hueRotation = 1.5,
+      this.startPoint = const Offset(200, 200)});
+  final logoSize;
+  static const _diameter = 307;
   @override
   void paint(Canvas canvas, Size size) {
     final height = size.height;
@@ -16,25 +60,31 @@ final Color color;
     Paint paint = Paint();
     paint.color = color;
 
-
     Path mainBackground = Path();
     mainBackground.addRect(Rect.fromLTRB(0, 0, width, height));
- 
-    Offset startPoint = new Offset(size.width/2, 0);
-    for (var i = 0; i < 3; i++){
-      var moveX = 50.0 * i;
-      var moveY = 50.0*i;
-      startPoint = new Offset(startPoint.dx + moveX, startPoint.dy + moveY);
 
-      canvas.drawCircle(startPoint, 50, paint);
-      var color = TinyColor(paint.color).darken(3);
-      paint.color = color.color;
+    var index = 0;
+
+    var hsv = HSLColor.fromColor(paint.color);
+    var starthue = (hsv.hue + (hueRotation * logoPoints.length)) % 360;
+    hsv = HSLColor.fromAHSL(hsv.alpha, starthue, hsv.saturation, 1);
+    for (var point in logoPoints) {
+      var offsetPoint = new Offset(startPoint.dx + (point.dx * logoSize),
+          startPoint.dy + (point.dy * logoSize));
+
+      var rotated = HSVColor.fromAHSV(
+          hsv.alpha,
+          ((hsv.hue - (hueRotation * index)) % 360),
+          hsv.saturation,
+          hsv.lightness);
+      paint.color = rotated.toColor();
+      canvas.drawCircle(offsetPoint, (_diameter * logoSize), paint);
+      index++;
     }
-
   }
 
   @override
   bool shouldRepaint(CustomPainter oldDelegate) {
     return oldDelegate != this;
   }
-  }
+}
--- lib/pages/auth/register.page.dart
+++ lib/pages/auth/register.page.dart
@@ -1,7 +1,7 @@
 import 'package:app/components/Buttons/roundedButton.dart';
+import 'package:app/components/Common/shadow_text.dart';
 import 'package:flutter/material.dart';
 import 'package:app/components/Common/bg_painter.dart';
-
 
 class RegisterPage extends StatefulWidget {
   @override
@@ -18,6 +18,8 @@ class _RegistrationData {
 
 class _RegisterPageState extends State<RegisterPage> {
   PageController controller;
+  HSVColor color = HSVColor.fromColor(Colors.orange);
+  Offset animOffset = Offset(0.0, 0.0);
   final FocusNode passwordFocusNode = FocusNode();
   _RegistrationData data = new _RegistrationData();
   @override
@@ -25,59 +27,217 @@ class _RegisterPageState extends State<RegisterPage> {
     // TODO: implement initState
     super.initState();
     controller = new PageController();
+    controller.addListener(() {
+      setState(() {
+        animOffset = Offset(
+            controller.position.pixels / 3, controller.position.pixels / 10);
+        color =
+            color.withHue((color.hue + controller.position.pixels / 300) % 360);
+      });
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-
     final Size screenSize = MediaQuery.of(context).size;
 
     return new Scaffold(
       body: Form(
-        child: PageView(
-          pageSnapping: true,
-          controller: controller,
-          physics: NeverScrollableScrollPhysics(),
-          children: <Widget>[
-            CustomPaint(
-              painter: BgPainter(),
-              child: Center(
-                
-                child: ListView(
-        shrinkWrap: true,
-        padding: EdgeInsets.only(left:24.0,right: 24.0),
-                  children: <Widget>[
-                    Text('YOUR NAME',style: Theme.of(context).textTheme.headline,),
-                       SizedBox(height: 48.0),
-                    new TextFormField(
-                        autofocus: true,
-                        keyboardType: TextInputType.emailAddress,
-                        decoration: new InputDecoration(
-                            hintText: 'your name', labelText: 'Name'),
+        child: CustomPaint(
+          painter: BgPainter(
+              color: color.toColor(),
+              logoSize: 1.0,
+              hueRotation: -1.2,
+              startPoint: Offset(
+                  screenSize.width * 0.2 + animOffset.dx, 270 + animOffset.dy)),
+          child: PageView(
+            pageSnapping: true,
+            controller: controller,
+            children: <Widget>[
+              namePage(screenSize, context),
+              emailPage(screenSize, context),
+              passwordPage(screenSize, context),
+              lawPage(screenSize, context)
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 
-                        onFieldSubmitted: (String textInput) {
-                          FocusScope.of(context)
-                              .requestFocus(passwordFocusNode);
-                        },
-                        onSaved: (String value) {
-                          data.email = value;
-                        }),
-                     SizedBox(height: 48.0),
-                new RoundedButton(
-                              buttonName: 'Next',
-                              onTap: () => controller.jumpToPage(2),
-                              buttonColor: Colors.transparent,
-                              borderWidth: 1.0,
-                              bottomMargin: 1.0,
-                              height: 50.0,
-                              width: screenSize.width,
-                            ),],
-                ),
+  Widget namePage(Size screenSize, BuildContext context) {
+    return Center(
+      child: ListView(
+        shrinkWrap: true,
+        padding: EdgeInsets.only(left: 24.0, right: 24.0),
+        children: <Widget>[
+          ShadowText(
+            'NAME',
+            style: Theme.of(context).textTheme.headline,
+            textScaleFactor: 1.0,
+            softWrap: true,
+          ),
+          SizedBox(height: 48.0),
+          new TextFormField(
+              autofocus: true,
+              keyboardType: TextInputType.emailAddress,
+              decoration:
+                  new InputDecoration(hintText: 'your name', labelText: 'Name'),
+              onFieldSubmitted: (String textInput) {
+                FocusScope.of(context).requestFocus(passwordFocusNode);
+              },
+              onSaved: (String value) {
+                data.email = value;
+              }),
+          SizedBox(height: 48.0),
+          new RoundedButton(
+            buttonName: 'Next',
+            onTap: () => controller.jumpToPage(2),
+            buttonColor: Colors.transparent,
+            borderWidth: 2.0,
+            bottomMargin: 1.0,
+            height: 50.0,
+            width: screenSize.width,
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget emailPage(Size screenSize, BuildContext context) {
+    return Center(
+      child: ListView(
+        shrinkWrap: true,
+        padding: EdgeInsets.only(left: 24.0, right: 24.0),
+        children: <Widget>[
+          ShadowText(
+            'EMAIL',
+            style: Theme.of(context).textTheme.headline,
+            textScaleFactor: 1.0,
+            softWrap: true,
+          ),
+          SizedBox(height: 48.0),
+          new TextFormField(
+              autofocus: true,
+              keyboardType: TextInputType.emailAddress,
+              decoration:
+                  new InputDecoration(hintText: 'your name', labelText: 'Name'),
+              onFieldSubmitted: (String textInput) {
+                FocusScope.of(context).requestFocus(passwordFocusNode);
+              },
+              onSaved: (String value) {
+                data.email = value;
+              }),
+          SizedBox(height: 48.0),
+          new RoundedButton(
+            buttonName: 'Next',
+            onTap: () => controller.jumpToPage(2),
+            buttonColor: Colors.transparent,
+            borderWidth: 2.0,
+            bottomMargin: 1.0,
+            height: 50.0,
+            width: screenSize.width,
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget passwordPage(Size screenSize, BuildContext context) {
+    return Center(
+      child: ListView(
+        physics: NeverScrollableScrollPhysics(),
+        shrinkWrap: true,
+        padding: EdgeInsets.only(left: 24.0, right: 24.0),
+        children: <Widget>[
+          ShadowText(
+            'PASSWORD',
+            style: Theme.of(context).textTheme.headline,
+            textScaleFactor: 1.0,
+            softWrap: true,
+          ),
+          SizedBox(height: 48.0),
+          new TextFormField(
+              autofocus: true,
+              keyboardType: TextInputType.emailAddress,
+              decoration: new InputDecoration(
+                  hintText: 'your name', labelText: 'Password'),
+              onFieldSubmitted: (String textInput) {
+                FocusScope.of(context).requestFocus(passwordFocusNode);
+              },
+              onSaved: (String value) {
+                data.email = value;
+              }),
+          new TextFormField(
+              autofocus: true,
+              keyboardType: TextInputType.emailAddress,
+              decoration: new InputDecoration(
+                  hintText: 'your name', labelText: 'Confirm password'),
+              onFieldSubmitted: (String textInput) {
+                FocusScope.of(context).requestFocus(passwordFocusNode);
+              },
+              onSaved: (String value) {
+                data.email = value;
+              }),
+          SizedBox(height: 48.0),
+          new RoundedButton(
+            buttonName: 'Next',
+            onTap: () => controller.jumpToPage(2),
+            buttonColor: Colors.transparent,
+            borderWidth: 2.0,
+            bottomMargin: 1.0,
+            height: 50.0,
+            width: screenSize.width,
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget lawPage(Size screenSize, BuildContext context) {
+    return Container(
+      child: CustomScrollView(
+        physics: NeverScrollableScrollPhysics(),
+        shrinkWrap: true,
+        slivers: <Widget>[
+          SliverAppBar(
+            expandedHeight: 300.0,
+            backgroundColor: Colors.transparent,
+            centerTitle: true,
+            flexibleSpace: FlexibleSpaceBar(
+              centerTitle: true,
+              title: ShadowText(
+                'TERM OF USSAGE',
+                style: Theme.of(context)
+                    .copyWith(
+                        textTheme: TextTheme(
+                            headline: TextStyle(
+                      fontSize: 25.0,
+                    )))
+                    .textTheme
+                    .headline,
+                maxLines: 2,
+                overflow: TextOverflow.ellipsis,
+                textScaleFactor: 1.0,
+                softWrap: true,
               ),
             ),
-            Placeholder()
-          ],
-        ),
+          ),
+          new SliverList(
+              delegate: new SliverChildListDelegate(<Widget>[
+            SizedBox(height: 48.0),
+            new Text(''),
+            new RoundedButton(
+              buttonName: 'ACCEPT & REGISTER',
+              onTap: () => controller.jumpToPage(2),
+              buttonColor: Colors.transparent,
+              borderWidth: 2.0,
+              bottomMargin: 1.0,
+              height: 50.0,
+              width: screenSize.width,
+            ),
+          ]))
+        ],
       ),
     );
   }
