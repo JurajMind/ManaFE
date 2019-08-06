f41509f4b3aa732ba99ecfc70f801c74475d2c3e
Juraj Hamornik <jurko@bdi.sk>
2019-08-06 16:43:23 +0200

    health page

---
+++ lib/components/Charts/gauge.dart
@@ -0,0 +1,143 @@
+import 'dart:math';
+
+/// Gauge chart example, where the data does not cover a full revolution in the
+/// chart.
+import 'package:charts_flutter/flutter.dart' as charts;
+import 'package:flutter/material.dart';
+
+class GaugeChart extends StatelessWidget {
+  final List<charts.Series> seriesList;
+  final bool animate;
+  final double value;
+  final int maxValue;
+  final int minValue;
+  final Color handColor;
+
+  GaugeChart(this.seriesList,
+      {this.animate, this.value, this.maxValue, this.minValue, this.handColor});
+
+  /// Creates a [PieChart] with sample data and no transition.
+  factory GaugeChart.withSampleData() {
+    return new GaugeChart(
+      _createSampleData(),
+      // Disable animations for image tests.
+      animate: false,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    var paint = new Paint();
+    paint.color = handColor ?? Colors.black87;
+    paint.style = PaintingStyle.fill;
+
+    return Stack(
+      children: <Widget>[
+        new charts.PieChart(seriesList,
+            animate: animate,
+            // Configure the width of the pie slices to 30px. The remaining space in
+            // the chart will be left as a hole in the center. Adjust the start
+            // angle and the arc length of the pie so it resembles a gauge.
+            defaultRenderer: new charts.ArcRendererConfig(
+                arcWidth: 30, startAngle: 4 / 5 * pi, arcLength: 7 / 5 * pi)),
+        Positioned.fill(
+          child: Align(
+              alignment: Alignment.center,
+              child: new CustomPaint(
+                size: new Size(200, 200),
+                foregroundPainter: new HandPainter(
+                  hours: 10,
+                  minutes: 10,
+                  hourHandPaint: paint
+                ),
+              )),
+        ),
+        Positioned.fill(
+          child: Align(
+              alignment: Alignment.center,
+              child: Text(
+                "$value/$maxValue",
+                style: Theme.of(context).textTheme.body2,
+              )),
+        ),
+      ],
+    );
+  }
+
+  /// Create one series with sample hard coded data.
+  static List<charts.Series<GaugeSegment, String>> _createSampleData() {
+    final data = [
+      new GaugeSegment('Low', 75),
+      new GaugeSegment('Acceptable', 100),
+      new GaugeSegment('High', 50),
+      new GaugeSegment('Highly Unusual', 5),
+    ];
+
+    return [
+      new charts.Series<GaugeSegment, String>(
+        id: 'Segments',
+        domainFn: (GaugeSegment segment, _) => segment.segment,
+        measureFn: (GaugeSegment segment, _) => segment.size,
+        areaColorFn: (GaugeSegment segment, _) =>  charts.Color(r: segment.size),
+        data: data,
+      )
+    ];
+  }
+}
+
+/// Sample data type.
+class GaugeSegment {
+  final String segment;
+  final int size;
+
+  GaugeSegment(this.segment, this.size);
+}
+
+class HandPainter extends CustomPainter {
+  final Paint hourHandPaint;
+  int hours;
+  int minutes;
+
+  HandPainter({
+    this.hours,
+    this.minutes,
+    this.hourHandPaint
+  });
+  
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final radius = size.width / 2;
+    // To draw hour hand
+    canvas.save();
+
+    canvas.translate(radius, radius);
+
+    //checks if hour is greater than 12 before calculating rotation
+    canvas.rotate(this.hours >= 12
+        ? 2 * pi * ((this.hours - 12) / 12 + (this.minutes / 720))
+        : 2 * pi * ((this.hours / 12) + (this.minutes / 720)));
+
+    Path path = new Path();
+
+    //hour hand stem
+    path.moveTo(-1.0, -radius + radius / 4);
+    path.lineTo(-5.0, -radius + radius / 2);
+    path.lineTo(-2.0, 0.0);
+    path.lineTo(2.0, 0.0);
+    path.lineTo(5.0, -radius + radius / 2);
+    path.lineTo(1.0, -radius + radius / 4);
+    path.close();
+
+    canvas.drawPath(path, hourHandPaint);
+    canvas.drawShadow(path, Colors.black, 2.0, false);
+
+    canvas.restore();
+  }
+
+  @override
+  bool shouldRepaint(HandPainter oldDelegate) {
+    return true;
+  }
+}
--- lib/pages/Statistic/health_page.dart
+++ lib/pages/Statistic/health_page.dart
@@ -1,5 +1,9 @@
+import 'package:app/components/Charts/gauge.dart';
 import 'package:app/const/theme.dart';
+import 'package:app/utils/translations/app_translations.dart';
+import 'package:charts_flutter/flutter.dart';
 import 'package:flutter/material.dart';
+import 'package:charts_common/common.dart' as chart;
 
 class HealthPage extends StatefulWidget {
   HealthPage({Key key}) : super(key: key);
@@ -8,12 +12,159 @@ class HealthPage extends StatefulWidget {
 }
 
 class _HealthPageState extends State<HealthPage> {
+  static List<Series<GaugeSegment, String>> _createSampleData() {
+    final data = [
+      new GaugeSegment('Low', 100),
+      new GaugeSegment('Acceptable', 75),
+      new GaugeSegment('High', 50),
+      new GaugeSegment('Highly Unusual', 25),
+    ];
+
+    final colors = [
+      chart.Color.fromHex(code: "#00CA43"),
+      chart.Color.fromHex(code: "#ECFF33"),
+      chart.Color.fromHex(code: "#FFBB33"),
+      chart.Color.fromHex(code: "#FF5733"),
+    ];
+
+    return [
+      new Series<GaugeSegment, String>(
+        id: 'Segments',
+        domainFn: (GaugeSegment segment, _) => segment.segment,
+        measureFn: (GaugeSegment segment, _) => segment.size,
+        colorFn: (GaugeSegment segment, index) => colors[index],
+        data: data,
+      )
+    ];
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: AppColors.black,
-        title: Text('Health page'),
+        title: Text('Healt'),
+        centerTitle: true,
+      ),
+      body: ListView(
+        children: <Widget>[
+          Card(
+            margin: EdgeInsets.all(8),
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                children: <Widget>[
+                  Text(
+                    AppTranslations.of(context).text('health.time'),
+                    style: Theme.of(context).textTheme.display1,
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Text(AppTranslations.of(context).text('health.time_text')),
+                       SizedBox(
+                    height: 10,
+                  ),
+                  Text('1day 2hours 31minutes',
+                      style: Theme.of(context).textTheme.display1)
+                ],
+              ),
+            ),
+          ),
+          Card(
+            margin: EdgeInsets.all(8),
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                children: <Widget>[
+                  Text(
+                    AppTranslations.of(context).text('health.last_hookah'),
+                    style: Theme.of(context).textTheme.display1,
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Text(AppTranslations.of(context)
+                      .text('health.last_hookah_text')),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Text('1day 2hours 31minutes',
+                      style: Theme.of(context).textTheme.display1)
+                ],
+              ),
+            ),
+          ),
+          Card(
+            margin: EdgeInsets.all(8),
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                children: <Widget>[
+                  Text(
+                    AppTranslations.of(context).text('health.co'),
+                    style: Theme.of(context).textTheme.display1,
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Text(AppTranslations.of(context).text('health.co_text')),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Container(
+                    height: 200,
+                    child: GaugeChart(_createSampleData(),minValue: 0,value: 7.6,maxValue: 10,handColor: AppColors.colors[0],),
+                  )
+                ],
+              ),
+            ),
+          ),
+          Card(
+            margin: EdgeInsets.all(8),
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                children: <Widget>[
+                  Text(
+                    AppTranslations.of(context).text('health.gas'),
+                    style: Theme.of(context).textTheme.display1,
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Text(AppTranslations.of(context).text('health.gas_text'))
+                ],
+              ),
+            ),
+          ),
+          Card(
+            margin: EdgeInsets.all(8),
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                children: <Widget>[
+                  Text(
+                    AppTranslations.of(context).text('health.nicotine'),
+                    style: Theme.of(context).textTheme.display1,
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Text(AppTranslations.of(context).text('health.nicotine_text')),
+                   Container(
+                    height: 200,
+                    child: GaugeChart(_createSampleData(),minValue: 0,value: 3.6,maxValue: 10,handColor: AppColors.colors[0]),
+                  )
+                ],
+              ),
+            ),
+          ),
+          
+          SizedBox(
+            height: 100,
+          )
+        ],
       ),
     );
   }
